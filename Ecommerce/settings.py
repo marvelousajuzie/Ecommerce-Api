f1c4ca6d9484d3464e4a05ad5884996cd36422e8
marvelousajuzie <Chizurummarvelous14@gmail.com>
2024-07-02 12:41:36 +0100

    work

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -90,27 +90,12 @@ if not DEBUG:
     }
     
 else:
-    
-
-
-        DATABASES = {
-            'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD'),
-            'HOST': config('DB_HOST'),
-            'PORT': 5432
-            }
-            }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    }
 
 
 # Password validation
