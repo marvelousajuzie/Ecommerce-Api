8d8f44faf6a9b8bb324a54ef0008279a9db4d2ec
marvelousajuzie <Chizurummarvelous14@gmail.com>
2024-07-02 12:33:19 +0100

    fix

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True')=="True"
+DEBUG = True
 
 
 
